47377be71bb0e1132693383dabf11a6f44280a3a
yunyoung <yunyoung1819@leznin.com>
2024-04-01 00:17:26 +0900

    :sparkle: feat: Add MemberShip Service

--- membership-service/src/main/java/com/zeropay/membership/MembershipApplication.java
+++ membership-service/src/main/java/com/zeropay/membership/MembershipApplication.java
@@ -1,11 +1,11 @@
-package org.example.zeropay;
+package com.zeropay.membership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class MembershipApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class, args);
+        SpringApplication.run(MembershipApplication.class, args);
     }
 }
--- membership-service/src/main/java/com/zeropay/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/zeropay/membership/adapter/in/web/RegisterMembershipController.java
@@ -1,13 +1,12 @@
-package org.example.zeropay;
+package com.zeropay.membership.adapter.in.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class RegisterMembershipController {
 
     @GetMapping(path = "/test")
     void test() {
