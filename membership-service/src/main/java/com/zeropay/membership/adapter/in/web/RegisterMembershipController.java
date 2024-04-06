262eb62371b8e4ed7e5807cd7da6541972718f2f
yunyoung <yunyoung1819@leznin.com>
2024-04-06 19:13:43 +0900

    :sparkle: feat: Add MemberShip Service 1

--- membership-service/src/main/java/com/zeropay/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/zeropay/membership/adapter/in/web/RegisterMembershipController.java
@@ -1,15 +1,18 @@
 package com.zeropay.membership.adapter.in.web;
 
+import common.WebAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@WebAdapter
 @RestController
 @RequiredArgsConstructor
 public class RegisterMembershipController {
 
-    @GetMapping(path = "/test")
-    void test() {
+    @PostMapping(path = "/register")
+    void registerMembership() {
         System.out.println("Hello world!");
     }
 }
